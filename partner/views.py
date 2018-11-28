bfb4f8cd860c3ad4ab288ec133416068511622c4
Chaeoh <limchyo>
2018-11-28 18:03:26 +0900

    made logout system

--- partner/views.py
+++ partner/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 
 # Create your views here.
@@ -23,6 +23,8 @@ def signup(request):
     return render(request, "signup.html", ctx)
 
 def login(request):
+    ctx = {}
+
     if request.method == "GET":
         pass
     elif request.method == "POST":
@@ -31,10 +33,14 @@ def login(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             auth_login(request, user)
+            return redirect("/partner/")
+
         else:
-            pass
+            ctx.update({ "error" : "존재하지 않는 사용자입니다." })
 
-        return redirect("/partner/")
 
-    ctx = {}
     return render(request, "login.html", ctx)
+
+def logout(request):
+    auth_logout(request)
+    return redirect("/partner/")
