e05fee283d5afc1243b866b9e9de0ad8ff7f4716
Chaeoh <limchyo>
2018-11-28 14:58:17 +0900

    made signup form

--- partner/views.py
+++ partner/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    ctx = {}
+    return render(request, "index.html", ctx)
+
+def signup(request):
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = User.objects.create_user(username, email, password)
+
+    ctx = {}
+    return render(request, "signup.html", ctx)
