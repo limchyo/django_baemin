b99194630232049c59bdee616f0cd08732ca6aaf
Chaeoh <limchyo>
2018-11-29 12:18:58 +0900

    made navbar

--- partner/models.py
+++ partner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Partner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    description = models.TextField()
