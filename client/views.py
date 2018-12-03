db07d4c29ff0aa1e84fbdea4378f0fe74171a7ed
Chaeoh <limchyo>
2018-12-03 10:20:41 +0900

    made client page

--- client/views.py
+++ client/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from partner.models import Partner
 
 # Create your views here.
+def index(request):
+    partner_list = Partner.objects.all()
+    ctx = {
+        "partner_list" : partner_list,
+    }
+    return render(request, "main.html", ctx)
