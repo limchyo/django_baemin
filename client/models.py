4e702ff7a966d22b67764890c9fde04b2ffe3830
Chaeoh <limchyo>
2018-12-06 17:46:36 +0900

    made Client&Order model class

--- client/models.py
+++ client/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from partner.models import Menu
 
 # Create your models here.
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    order_menu = models.ManyToManyField(Menu)
